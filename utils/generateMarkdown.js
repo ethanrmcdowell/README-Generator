834fcb49ca67a2651fa7205d86f13507160d9c2f
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-11-08 20:17:57 -0500

    readme test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  var githublink = ("<a href='https://github.com/" + `${data.github}` + "'>Github</a>");
   return `
   
   
   # ${data.title}
   # a project by ${data.contributors}
-  # <a href="https://github.com/" + ${data.github}>Github</a>
+  # ${githublink}
 
 
 
