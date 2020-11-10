6fcd737321f98b0d16ae8d479fa98164d463c8b9
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-11-10 14:51:35 -0500

    Fixing bug with alternative license name showing up

---
+++ hdjakshkjdas.md
@@ -0,0 +1,54 @@
+
+  
+  
+  # **hdjakshkjdas**
+
+  
+
+  hdkjashdkjashkjdsa
+
+  ## TABLE OF CONTENTS
+  
+  - [Installation](#Installation) 
+
+  - [Usage](#Usage) 
+
+  - [License](#License) 
+
+  - [Contributing](#Contributing) 
+
+  - [Tests](#Tests) 
+
+  - [Questions](#Questions) 
+
+  
+  ## Installation
+  
+ `hjkdsajkhdkjsahkj` 
+
+  ## Usage
+  
+ `hkjdsahkjdhkjashdkj` 
+
+  ## License
+  
+ Copyright 1988 Ethan R McDowell
+  
+ Licensed under IBM
+  
+
+  ## Contributing
+  
+ Ethan McDowell 
+
+  ## Tests
+  
+ ```var hdkjsahdkjsa = console.log("hdkjashkj");``` 
+
+  ## Questions
+  
+ :link: <a href='https://github.com/ethanrmcdowell'>ethanrmcdowell</a>
+  
+  
+ :e-mail: ethan.r.mcdowell@gmail.com
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,7 +7,7 @@ function generateMarkdown(data) {
   
   # **${data.title}**
 
-  ${data.license === "MIT License" ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.license === "Apache License v2.0" ? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.license === "GNU General Public License v3.0" ? "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)" : data.license === "Other" ? `${data.other}` : ""}
+  ${data.license === "MIT License" ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.license === "Apache License v2.0" ? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.license === "GNU General Public License v3.0" ? "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)" : ""}
 
   ${data.description}
 
@@ -26,7 +26,8 @@ function generateMarkdown(data) {
   \n ${data.usage} \n
   ## License
   \n Copyright ${data.year} ${data.name}
-  \n Licensed under ${data.license} \n
+  \n Licensed under ${data.license === "Other" ? `${data.other}` : data.license === "None" ? "" : data.license }
+  \n
   ## Contributing
   \n ${data.contributors} \n
   ## Tests
